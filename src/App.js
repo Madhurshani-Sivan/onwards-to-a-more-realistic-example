8b1522c76ea15e043dc758b5dd4cd4c98b567f73
madhurshani-sivan <madhurshanisivan@gmail.com>
2023-07-27 17:54:23 +0530

    prevent infinite looping due to hook dependencies

--- src/App.js
+++ src/App.js
@@ -7,30 +7,26 @@ import useHttp from "./hooks/use-http";
 function App() {
   const [tasks, setTasks] = useState([]);
 
-  const tranformTasks = (tasksObj) => {
-    const loadedTasks = [];
-
-    for (const taskKey in tasksObj) {
-      loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
-    }
-
-    setTasks(loadedTasks);
-  };
-
-  const {
-    isLoading,
-    error,
-    sendRequest: fetchTasks,
-  } = useHttp(
-    {
-      url: "https://react-http-ca029-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json",
-    },
-    tranformTasks
-  );
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
 
   useEffect(() => {
-    fetchTasks();
-  }, []);
+    const tranformTasks = (tasksObj) => {
+      const loadedTasks = [];
+
+      for (const taskKey in tasksObj) {
+        loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+      }
+
+      setTasks(loadedTasks);
+    };
+
+    fetchTasks(
+      {
+        url: "https://react-http-ca029-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json",
+      },
+      tranformTasks
+    );
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
